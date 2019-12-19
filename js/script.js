6821cbb4d9bb046b42a8d5e9be22d2bd2d118701
AaronBrs <brosseau.aaron@gmail.com>
2019-12-19 12:28:54 +0100

    Ajout de vérification des questions pour le quizz

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
+
+let listePersonnages = new Array('Grace Burgess','Polly Gray','Arthur Shelby','Michael Gray','John Shelby');
+let indicePersonnages = new Array(1,3,0,2,4);
+
 function FormValidation(){
+    VerifQuestion1();
+    VerifQuestion4();
 
 }
 function SwitchPic(){
-
 }
 const ensembleReponse1 = new Set(["Thomas shelby","thomas Shelby", "Thomas Shelby","THOMAS SHELBY","thomas shelby"]);
 
 function VerifQuestion1(){
     reponse=document.getElementById("personnageQuestion1").value;
-    console.log(reponse);
     if (ensembleReponse1.has(reponse)){
         document.getElementById("personnageQuestion1").className='LabelTextCorrect';
     }
@@ -16,3 +20,33 @@ function VerifQuestion1(){
         document.getElementById("personnageQuestion1").className='LabelTextWrong';
     }
 }
+function VerifQuestion2(){
+    reponse=document.getElementById("ANG").value;
+    if (reponse=="Angleterre"){
+        
+    }
+    /*
+    if (document.getElementById("ANG").checked){
+
+    }
+    */
+}
+function VerifQuestion4(){
+    for (let i=0; i<listePersonnages.length; i++){
+      if((document.forms['Questionnaire'].elements['ListePerso'+(i+1)].selectedIndex-1)==indicePersonnages[i]){
+        document.getElementById('PersoNumero'+(i+1)).className='ValeurListeCorrect';
+      }
+      else{
+        document.getElementById('PersoNumero'+(i+1)).className='ValeurListeIncorrect';
+      }
+    }
+}
+function selectionPersonnage(s0){
+    for(let i=0;i<listePersonnages.length;i++){//vérification sur toutes les valeurs de la liste
+      let s=document.forms['Questionnaire'].elements['ListePerso'+(i+1)]; //s prend la valeur un élément de la liste
+      if ((s!=s0)&&(s.options[s.selectedIndex].text==s0.options[s0.selectedIndex].text)){//si s est déjà dans la liste
+        s.selectedIndex=0;//réinitialise s
+      }
+    }
+  }
+  
